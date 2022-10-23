563293ccfd773f4f669d036bb40a2d7ae1b285a3
nikolay <nikolay.mal88@mail.ru>
2022-10-23 19:29:09 +0400

    ДЗ 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* Случайное число */
 const generateRandomInt = (min,max) => {
   if (min < 0 || max < 0 || min === max) {
@@ -20,3 +21,45 @@ const checkLength = (line, maxlength) =>
 
 
 checkLength('строка',6);
+
+
+const DESCRIPTIONS = [
+  'Наше фото',
+  'Мое фото',
+  'Общее фото',
+  'Неудачное фото',
+  'Лучшее фото',
+  'Фото',
+  'Главное фото'
+];
+
+// массивы с числами от 1 до 25
+const NUMBER_ID = [];
+const NUMBER_URL = [];
+for (let i = 1; i <= 25; i++) {
+  NUMBER_ID.push(i);
+  NUMBER_URL.push(i);}
+
+const createFoto = () => {
+  const idNumber = NUMBER_ID.shift();
+  const UrlNumber = NUMBER_URL.shift();
+  const randomDescriptionNumber = generateRandomInt(0, DESCRIPTIONS.length - 1);
+  const randomIikesNumber = generateRandomInt(15,200);
+  const randomCommentsNumber = generateRandomInt(0,200);
+
+
+  return {
+    id: idNumber,
+
+    url: `photos/${ UrlNumber }.jpg`,
+
+    description: DESCRIPTIONS[randomDescriptionNumber],
+
+    likes: randomIikesNumber,
+
+    comments: randomCommentsNumber
+  };
+};
+
+const listFoto = Array.from({length: 25}, createFoto);
+console.log(listFoto);
