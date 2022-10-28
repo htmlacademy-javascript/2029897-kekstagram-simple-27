b72dc9e2d97100fe7fa62c2dac25bcd38f76072d
nikolay <nikolay.mal88@mail.ru>
2022-10-28 20:50:01 +0400

    ДЗ 4 после замечаний

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+
 /* Случайное число */
 const generateRandomInt = (min,max) => {
   if (min < 0 || max < 0 || min === max) {
@@ -33,25 +33,16 @@ const DESCRIPTIONS = [
   'Главное фото'
 ];
 
-// массивы с числами от 1 до 25
-const NUMBER_ID = [];
-const NUMBER_URL = [];
-for (let i = 1; i <= 25; i++) {
-  NUMBER_ID.push(i);
-  NUMBER_URL.push(i);}
-
-const createFoto = () => {
-  const idNumber = NUMBER_ID.shift();
-  const UrlNumber = NUMBER_URL.shift();
+const createPhoto = (_elem, id) => {
+  const idNumber = ++id;
   const randomDescriptionNumber = generateRandomInt(0, DESCRIPTIONS.length - 1);
   const randomIikesNumber = generateRandomInt(15,200);
   const randomCommentsNumber = generateRandomInt(0,200);
 
-
   return {
     id: idNumber,
 
-    url: `photos/${ UrlNumber }.jpg`,
+    url: `photos/${ idNumber}.jpg`,
 
     description: DESCRIPTIONS[randomDescriptionNumber],
 
@@ -61,5 +52,5 @@ const createFoto = () => {
   };
 };
 
-const listFoto = Array.from({length: 25}, createFoto);
-console.log(listFoto);
+const listPhoto = Array.from({length: 25}, createPhoto);
+console.log(listPhoto);
